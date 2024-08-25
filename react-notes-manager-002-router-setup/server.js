9e5d4f01de2e724896089adb8ed3e689f6f02272
Andranik Virabyan <gmparstone99@gmail.com>
2024-08-25 10:21:54 -0800

    hh

--- react-notes-manager-002-router-setup/server.js
+++ react-notes-manager-002-router-setup/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./react-notes-manager-002-router-setup/db.json'); // Path to your db.json file
+const router = jsonServer.router('./db.json'); // Path to your db.json file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
